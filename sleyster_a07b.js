ec309b65f8c43fae479049753f7365662d42dc76
hsleyster <58783834+hsleyster@users.noreply.github.com>
2020-02-04 11:53:29 -0600

    Assignment 07

--- sleyster_a07b.js
+++ sleyster_a07b.js
@@ -19,8 +19,8 @@ console.log(getPrompt(groceryItems));
 //Step 1
 
 //function buildFoodsList invoked
-buildFoodsList (badFoods, "bread", "beer", "popcorn", "bacon", "soda");        
-
+buildFoodsList(badFoods, "bread", "beer", "popcorn", "bacon", "soda");        
+console.log("bad foods is built");
 standardInput.on('data', function (text) {
     //This line will remove line breaks 
     //\n\r in window, \r in macOS
@@ -28,39 +28,32 @@ standardInput.on('data', function (text) {
     //Step 4 Begin
    
     //if statement to check for exit value if true exit
-if ( text === "Q" || "q" ) {
+if ( text == "Q" || text == "q" ) {
     clearScreen();
     console.log("Bye");
     process.exit();
 }
-   //else if statement to check for exit value if false then invoke the checkItem function 
-else if (text != "Q" || "q" ) {
+//else if statement to check value for view list if true displays grocery list
+else if (text == "V" || text == "v" ) {
     clearScreen();
-    checkItem(text, badFoods);
+    displayList(groceryItems);
+    getPrompt(groceryItems);
 }
     //if statement to check value of checkItem function if true adds item to list and asks for more input
-if (checkItem(text, badFoods) != true) {
+else if (checkItem(text, badFoods) != true) {
     formatItem(text);
-    groceryList.push(text);
-    console.log( `Item added to list! /n
-    List contains ${groceryList.length} items. /n
-    Please enter a grocery item. /n
-    Enter Q to quit.`);
+    groceryItems.push(text);
+    console.log( `Item added to list! 
+    List contains ${groceryItems.length} items.`);
+    getPrompt(groceryItems);
 }
-    //else if statement to check value for view list if true displays grocery list
-else if (text === "V" || "v" ) {
-    clearScreen();
-    displayList(groceryList);
-    getPrompt(groceryList);
-}
+    
     //else statement to check value of checkItem function if false outputs error message and asks for more input
 else { console.log( `Error: that item is not safe for your allergies.
-It has not been added to your list. /n
-Your grocery list contains ${groceryList.length} items. /n
-Please enter a grocery item. /n
-Enter Q to quit.`);
-getPrompt(groceryList);
-console.log(groceryList);
+It has not been added to your list. 
+Your grocery list contains ${groceryItems.length} items.`);
+getPrompt(groceryItems);
+console.log(groceryItems);
 }
     //Step 4 End
 });
@@ -70,35 +63,35 @@ console.log(groceryList);
 //Step 2
 
 //function buildFoodsList defined to fill the badFoos array with user inputed items
-function buildFoodsList (badFoods, ...args) {
+function buildFoodsList(foods, ...args) {
     for (arg of args) {
-        badFoods.unshift(arg);
+        foods.unshift(arg);
+        console.log("we are in the for loop");
     }
 }
 //function getPrompt defined to output prompt for user to input items or quit or view items
-function getPrompt (groceryList) {
-    console.log(`Your grocery list contains ${groceryList.length} items. 
+function getPrompt(grocery) {
+    console.log(`Your grocery list contains ${grocery.length} items. 
     Please enter a grocery item.
-    Enter Q to quit./n Enter V to view list.`);
+    Enter Q to quit.
+    Enter V to view list.`);
 }
 //function displayList defined to display grocery list
-function displayList (groceryList) {
+function displayList(groceryItems) {
     console.log("Grocery List");
-    console.log( `(${groceryList.length} items)`);
-    for (let i = 0; max = groceryList.length; i < max, i++) {
-        console.log(`${i}). ${groceryList[i]} `);
+    console.log( `(${groceryItems.length} items)`);
+    for (let i = 0, max = groceryItems.length; i < max; i++) {
+        console.log(`${i+1}). ${groceryItems[i]} `);
     }
 }
 //Step 3
 
 //function checkItem defined to check if item entered is not in the badFoods array
-function checkItem (string, badFoods) {
-    badFoods.toLowerCase();
-    badFoods.includes(string);
-    badFoods.toUpperCase(index0);
+function checkItem(string, badFoods) {
+    return badFoods.includes(string);
 }
 //function formatItem defined to format user inputed items
-function formatItem (string) {
+function formatItem(string) {
     string.trim();
 }
 
